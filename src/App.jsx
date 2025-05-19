46a20c9439d46bc63b4f9d63767c84b040990788
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-19 20:29:01 +0200

    [DONE]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Routes Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
 import ProductDetail from './pages/ProductDetail'; 
-import Navbar from './components/Navbar
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
